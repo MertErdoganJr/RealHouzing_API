5f80458f4ffe3f21bd397e4c75e5dcab5124d47d
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-06-06 19:52:04 +0300

    DefaultPage_Api_Connect

---
+++ RealHouizng.Consume/ViewComponents/Default/_PropertiesPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealHouzing.DtoLayer.ProductDtos;
+using System.ComponentModel;
+
+namespace RealHouizng.Consume.ViewComponents.Default
+{
+    public class _PropertiesPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _PropertiesPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44369/api/Product");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
